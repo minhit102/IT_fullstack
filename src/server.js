039d6183fca3601dc82e2faf1f3ea89b4e775125
oop <minhit102k66@gmail.com>
2023-08-25 01:52:05 +0700

    .

--- src/server.js
+++ src/server.js
@@ -2,14 +2,16 @@ const express = require('express')
 const path = require('path');
 const app = express()
 const port = 8080
-const hostname = 'locahost'
+const hostname = 'localhost'
+require('dotenv').config()
 console.log(process.env)
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-  res.render('sample.js')
+app.get('/hoidanit', (req, res) => {
+  //res.send('Hello World!123')
+  res.render('sample.ejs')
 })
 
 app.listen(port,hostname, () => {
