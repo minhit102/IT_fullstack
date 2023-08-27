749a6601db26693510e5b8cdc7058f2394e16010
oop <minhit102k66@gmail.com>
2023-08-28 00:08:14 +0700

    bai22

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const path = require('path');
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web')
+const mysql = require('mysql2');
 
 const app = express()
 const port = process.env.port;
@@ -14,6 +15,23 @@ configViewEngine(app)
 app.use('/test',webRoutes);
 app.use('/v1',webRoutes);
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port : 3307,
+  user: 'root',
+  password: '123456',
+  database: 'hoidanit'
+});
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected")
+})
+connection.query(
+  'SELECT * FROM Users;',
+  (err,results,fail) => {
+    console.log(results);
+  }
+)
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs')
 
